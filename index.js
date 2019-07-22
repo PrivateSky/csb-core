334f95d4c99dbf85cb82f2be529bfc799be3eb79
skutner <sava.dumitru.daniel@gmail.com>
2019-07-22 14:16:26 +0300

    removed EDFSBrickStorage.js

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ module.exports.EDFS = EDFS;
 module.exports.CSBIdentifier = CSBIdentifier;
 module.exports.FileHandler = FileHandler;
 module.exports.EDFSMiddleware = require("./EDFSMiddleware");
-module.exports.createEDFSBrickStorage = require("./lib/EDFSBrickStorage").createEDFSBrickStorage;
 
 

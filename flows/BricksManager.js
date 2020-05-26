27628672d59ae82b9cf0adc104b6c622965077ae
Vlad Balmos <vlad.balmos@code932.com>
2020-05-26 10:29:05 +0300

    Fix a bug in downloadMultipleBricks
    If a single hash is sent in the query, the req.query.hashes property
    returns a string instead of an array and breaks the business logic.
    This change converts the string to an array<string>

--- flows/BricksManager.js
+++ flows/BricksManager.js
@@ -51,6 +51,9 @@ $$.flow.describe("BricksManager", {
     },
 
     readMultipleBricks: function (brickHashes, writeStream, callback) {
+        if (!Array.isArray(brickHashes)) {
+            brickHashes = [brickHashes];
+        }
         this.__writeMultipleBricksToStream(brickHashes, 0, writeStream, callback);
     },
 
